a5b5989cf063253285df85bb6013e41363a1819c
pitaemir <emirbraz.d2@gmail.com>
2024-06-10 11:56:19 -0300

    adicionando user.models

--- tsfApp/models.py
+++ tsfApp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class user(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    donation_value = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.phone
+
+class member(user):
+    created_at = models.DateTimeField(auto_now_add=True)
