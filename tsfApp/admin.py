bef69872d1a583635726da0222c6dea0d6fc2083
pitaemir <emirbraz.d2@gmail.com>
2024-06-10 15:25:25 -0300

    migrations

--- tsfApp/admin.py
+++ tsfApp/admin.py
@@ -8,4 +8,10 @@ from .models import event
 from .models import donation
 
 # Register your models here.
+admin.site.register(user)
+admin.site.register(member)
+admin.site.register(visitor)
+admin.site.register(comment)
 admin.site.register(post)
+admin.site.register(event)
+admin.site.register(donation)
